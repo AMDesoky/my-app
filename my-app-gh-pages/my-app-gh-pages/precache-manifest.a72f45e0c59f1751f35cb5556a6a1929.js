b31d8711c1568a31efd346b0a84eaa58cdd7b5f5
Ahmed Desoky <100020770+AMDesoky@users.noreply.github.com>
2024-08-16 14:04:37 +0300

    Add files via upload

---
+++ my-app-gh-pages/my-app-gh-pages/precache-manifest.a72f45e0c59f1751f35cb5556a6a1929.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e97f4e3faa8b20f496bd",
+    "url": "/Ov3rControl/my-app/static/js/runtime~main.e97f4e3f.js"
+  },
+  {
+    "revision": "c275329b80caba83f49e",
+    "url": "/Ov3rControl/my-app/static/js/main.c275329b.chunk.js"
+  },
+  {
+    "revision": "1b9e583d730deb15f40e",
+    "url": "/Ov3rControl/my-app/static/js/1.1b9e583d.chunk.js"
+  },
+  {
+    "revision": "c275329b80caba83f49e",
+    "url": "/Ov3rControl/my-app/static/css/main.239ab4de.chunk.css"
+  },
+  {
+    "revision": "1b9e583d730deb15f40e",
+    "url": "/Ov3rControl/my-app/static/css/1.a0d0e350.chunk.css"
+  },
+  {
+    "revision": "45ed2415aebd1a05a461b72c8ccb37cf",
+    "url": "/Ov3rControl/my-app/index.html"
+  }
+];
